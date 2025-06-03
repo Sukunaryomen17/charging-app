eefcb0eae7d4a9c00e263d1d7b4f24788621ec8e
Sukunaryomen17 <aadijain448@gmail.com>
2025-06-03 09:29:48 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://charging-app-yccv.onrender.com',
+  credentials: true // Optional: only if using cookies/auth headers
+}));
 app.use(express.json());
 
 // MongoDB connection
